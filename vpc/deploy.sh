22a4015eb65b31e46420706f6579e2bce2be7c9e
Kristi <kristi.kristo@softup.co>
2020-06-10 18:11:36 +0200

    typo

--- vpc/deploy.sh
+++ vpc/deploy.sh
@@ -9,7 +9,7 @@ AWS_REGION='eu-central-1'
 
 aws cloudformation deploy \
   --template-file ./stack.yml \
-  --region $AWS_REGION
+  --region $AWS_REGION \
   --stack-name $VPC_NAME-vpc-$ENVIRONMENT \
   --parameter-overrides Environment=$ENVIRONMENT \
                         VpcName=$VPC_NAME
