13c5a49435365f6e41becef507c27cc901a20ac2
Kristi <kristi.kristo@softup.co>
2020-06-10 21:30:32 +0200

    Added bucket for artifacts and authorized keys (bastion host)

---
+++ authorized-keys/s3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+PROJECT_NAME='serverless-reference-infrastructure'
+ENVIRONMENT='dev'
+AWS_REGION='eu-central-1'
+
+aws cloudformation deploy \
+  --region $AWS_REGION \
+  --profile ${AWS_PROFILE:-default} \
+  --template-file ./stacks/s3-stack.yml \
+  --stack-name $PROJECT_NAME-bucket-authorized-keys-$ENVIRONMENT \
+  --no-fail-on-empty-changeset \
+  --parameter-overrides Environment=$ENVIRONMENT \
+                        ProjectName=$PROJECT_NAME
