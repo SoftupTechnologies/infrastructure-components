c03a6c45d9d1b5ed1fe1747e8849d92e21bfaea2
Stivi Ndoni <stivi.ndoni@gmail.com>
2020-06-11 17:54:59 +0200

    Restricting s3 access only available through cdn

--- s3/clientapp-bucket/deploy_oai.sh
+++ s3/clientapp-bucket/deploy_oai.sh
@@ -7,6 +7,6 @@ AWS_REGION='eu-central-1'
 aws cloudformation deploy \
   --template-file ./cdn_cf_origin_access_identity.yml \
   --region $AWS_REGION \
-  --stack-name $BUCKET_NAME-s3-client-app-$ENVIRONMENT \
+  --stack-name $OAI_COMMENT-s3-client-app-$ENVIRONMENT \
   --parameter-overrides OAIComment=$OAI_COMMENT \
                         Environment=$ENVIRONMENT
