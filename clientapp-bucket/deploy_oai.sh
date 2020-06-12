fdc32c9c0aab26e243fb8a44f5261fa89e4ae9f6
Stivi Ndoni <stivi.ndoni@gmail.com>
2020-06-12 14:28:04 +0200

    Organised dirs

--- clientapp-bucket/deploy_oai.sh
+++ clientapp-bucket/deploy_oai.sh
@@ -5,7 +5,7 @@ ENVIRONMENT=$2
 AWS_REGION='eu-central-1'
 
 aws cloudformation deploy \
-  --template-file ./cdn_cf_origin_access_identity.yml \
+  --template-file ./stacks/cdn_cf_origin_access_identity.yml \
   --region $AWS_REGION \
   --stack-name $OAI_COMMENT-s3-client-app-$ENVIRONMENT \
   --parameter-overrides OAIComment=$OAI_COMMENT \
