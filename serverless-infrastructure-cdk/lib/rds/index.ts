fe0f3402dab42b0b0b5bf5ee6893ce4e8a07d52f
Stivi Ndoni <stivi.ndoni@gmail.com>
2020-06-22 18:49:30 +0200

    Base rds stack

---
+++ serverless-infrastructure-cdk/lib/rds/index.ts
@@ -0,0 +1,42 @@
+import * as cdk from '@aws-cdk/core';
+import * as rds from '@aws-cdk/aws-rds';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import { Envs } from '../../types/envs';
+
+export interface RdsInfrastructureProps {
+  projectName: string,
+  env: Envs,
+  clientName: string,
+  dbMasterUserName: string,
+  vpc: ec2.Vpc,
+  databaseName: string,
+  cidrForIngressTraffic: string,
+}
+
+export class RdsInfrastructure extends cdk.Construct {
+  public readonly instance: rds.DatabaseInstance;
+
+  constructor(scope: cdk.Construct, id: string, props: RdsInfrastructureProps) {
+    super(scope, id);
+
+    // const rdsSg = new ec2.SecurityGroup(this, 'RdsSg', {
+
+    // });
+
+    const rdsSg = new ec2.SecurityGroup(this, 'RdsSg', {
+      vpc: props.vpc,
+      description: 'Rds security group',
+    });
+
+    rdsSg.addIngressRule(ec2.Peer.ipv4(props.cidrForIngressTraffic), ec2.Port.tcp(5439));
+
+    this.instance = new rds.DatabaseInstance(this, 'RdsDbInstance', {
+      engine: rds.DatabaseInstanceEngine.POSTGRES,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      masterUsername: `${props.projectName}-${props.dbMasterUserName}-${props.env}`,
+      vpc: props.vpc,
+      databaseName: props.databaseName,
+      securityGroups: [rdsSg],
+    });
+  }
+}
--- serverless-infrastructure-cdk/lib/serverless-infrastructure-cdk-stack.ts
+++ serverless-infrastructure-cdk/lib/serverless-infrastructure-cdk-stack.ts
@@ -5,12 +5,8 @@ import { ArtifactsBucket } from './artifacts-bucket';
 import { MyVpc } from './vpc';
 import { BastionHostServices } from './bastion-host';
 import { ClientAppInfrastructure } from './clientapp-bucket';
-
-export enum Envs {
-  DEV = 'dev',
-  STAGE = 'stage',
-  PROD = 'prod',
-}
+import { RdsInfrastructure } from './rds';
+import { Envs } from '../types/envs';
 
 interface StackProps {
   projectName: string,
@@ -36,37 +32,43 @@ export class ServerlessInfrastructureCdkStack extends cdk.Stack {
     //   ]
     // });
 
-    // const myVpc = new MyVpc(this, `MyVpc-${props.env}`, {
-    //   vpcCidr: '10.0.0.0/16',
-    //   publicSubnetProps: [
-    //     {
-    //       cidr: '10.0.5.0/24',
-    //       az: 'eu-central-1a',
-    //       mapPublicIpOnLaunch: true,
-    //       withNatGateway: true,
-    //       withBastionHost: true,
-    //     }
-    //   ],
-    //   privateSubnetProps: [
-    //     {
-    //       cidr: '10.0.6.0/24',
-    //       az: 'eu-central-1a',
-    //     }
-    //   ],
-    //   tags: [
-    //     {
-    //       key: 'cost-center',
-    //       value: props.clientName,
-    //     },
-    //     {
-    //       key: 'project',
-    //       value: props.projectName,
-    //     }
-    //   ]
+    const myVpc = new MyVpc(this, `MyVpc-${props.env}`, {
+      vpcCidr: '10.0.0.0/16',
+      publicSubnetProps: [
+        {
+          cidr: '10.0.5.0/24',
+          az: 'eu-central-1a',
+          mapPublicIpOnLaunch: true,
+          withNatGateway: true,
+          withBastionHost: true,
+        }
+      ],
+      privateSubnetProps: [
+        {
+          cidr: '10.0.6.0/24',
+          az: 'eu-central-1a',
+        }
+      ],
+      tags: [
+        {
+          key: 'cost-center',
+          value: props.clientName,
+        },
+        {
+          key: 'project',
+          value: props.projectName,
+        }
+      ]
+    });
+
+    // const caInfrastructure = new ClientAppInfrastructure(this, 'ClientAppInfrastructure', {
+    //   clientAppBucketName: `${props.projectName}-client-app-bucket-${props.clientName}-${props.env}`,
     // });
 
-    const caInfrastructure = new ClientAppInfrastructure(this, 'ClientAppInfrastructure', {
-      clientAppBucketName: `${props.projectName}-client-app-bucket-${props.clientName}-${props.env}`,
+    const database = new RdsInfrastructure(this, 'Rds', {
+      ...props,
+      dbMasterUserName: 'db',
+      vpc: myVpc.vpc,
     });
 
     // new cdk.CfnOutput(this, 'VpcId', {
